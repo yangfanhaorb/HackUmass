7553c71a26e121333cc49fcb04242a1c93a42911
yangfanhaorb <yangfanhaorb@gmail.com>
2015-04-11 22:46:56 -0400

    did some new activities

--- app/src/main/java/edu/amherst/fyang17/carpool/AddNew.java
+++ app/src/main/java/edu/amherst/fyang17/carpool/AddNew.java
@@ -1,32 +1,24 @@
 package edu.amherst.fyang17.carpool;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 
-public class TripDetail extends ActionBarActivity {
+public class AddNew extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_detail);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-        setContentView(textView);
+        setContentView(R.layout.activity_add_new);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_trip_detail, menu);
+        getMenuInflater().inflate(R.menu.menu_add_new, menu);
         return true;
     }
 
@@ -38,7 +30,6 @@ public class TripDetail extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-
 
         return super.onOptionsItemSelected(item);
     }
